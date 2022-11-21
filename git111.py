1318bbfc505e030456c688984e27d714a98a8779
PolinaBack <appolina14@mail.ru>
2022-11-21 16:59:11 +0300

    Класс

--- git111.py
+++ git111.py
@@ -1,6 +1,8 @@
 def main():
     print("voq gthdsq ghjtrn qit")
     print("Я меняю свой код кажждый день")
+    print('Опять новое добавление ')
+    print("bvjhgfchk")
 
 if __name__ == '__main__':
     main()
