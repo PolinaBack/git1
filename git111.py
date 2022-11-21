4362c36f1ba8000b2ff06da76ff4019fa876ba70
PolinaBack <appolina14@mail.ru>
2022-11-21 17:01:33 +0300

    вац руцпр

--- git111.py
+++ git111.py
@@ -3,6 +3,7 @@ def main():
     print("Я меняю свой код кажждый день")
     print('Опять новое добавление ')
     print("bvjhgfchk")
+    print("полседний принт на сегодня")
 
 if __name__ == '__main__':
     main()
